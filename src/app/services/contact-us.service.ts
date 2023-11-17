c07b30fd784e26454b0fb15f51f47acca7aeda36
edo99pal <edo99pal@gmail.com>
2023-11-17 19:36:30 +0100

    Little fix on the email of contact us

--- src/app/services/contact-us.service.ts
+++ src/app/services/contact-us.service.ts
@@ -7,28 +7,28 @@ export class ContactUsService {
   contactInformation = [
     {
       city: "Berlin",
-      email: "berlin@hostelchain.com",
+      email: "berlin@urbannest.com",
       phoneNumber: "+49 30 1234 5678",
       address: "123 Berlin Street, Berlin, Germany",
       operatingHours: "Monday to Sunday: 9:00 AM - 6:00 PM"
     },
     {
       city: "Prague",
-      email: "prague@hostelchain.com",
+      email: "prague@urbannest.com",
       phoneNumber: "+420 123 456 789",
       address: "456 Prague Avenue, Prague, Czech Republic",
       operatingHours: "Monday to Sunday: 9:00 AM - 6:00 PM"
     },
     {
       city: "Vienna",
-      email: "vienna@hostelchain.com",
+      email: "vienna@urbannest.com",
       phoneNumber: "+43 1 2345 6789",
       address: "789 Vienna Road, Vienna, Austria",
       operatingHours: "Monday to Sunday: 9:00 AM - 6:00 PM"
     },
     {
       city: "Budapest",
-      email: "budapest@hostelchain.com",
+      email: "budapest@urbannest.com",
       phoneNumber: "+36 1 234 5678",
       address: "101 Budapest Boulevard, Budapest, Hungary",
       operatingHours: "Monday to Sunday: 9:00 AM - 6:00 PM"
