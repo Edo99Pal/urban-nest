20c31764d71b2eee3527adbb50ac84669f3da7c4
edo99pal <edo99pal@gmail.com>
2023-12-10 15:13:08 +0100

    Deleted booking interface

--- src/app/models/booking.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Booking {
-    fullName: string | null,
-    location: string | null,
-    roomType: string | null,
-    nOfGuests: number | null,
-    startDate: Date | null,
-    endDate: Date | null,
-    breakfast: boolean | null
-}
